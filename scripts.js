f5fac8f075e0ff5c29efa6d279674e65f4ee9648
Franco Silva <francosilva@Francos-MacBook-Pro.local>
2020-02-04 10:35:21 -0600

    newer commit

--- scripts.js
+++ scripts.js
@@ -1,30 +1,40 @@
-var friendName = ['Blake', 'Kayla', 'Cole', 'Mary', 'Josh'];
-var locationName = ['Hoover','Bessemer','Homewood', 'Mountion Brook','Pelham', 'McCalla','Vestavia Hills','Alabaster','Hueytown','Trussville'];
-var weaponName = ['Sword','Spear','Hammer','Mace','Fist','Bow','dagger','Sheild','Rod','Staff','Cannons','Whip','Bomb','Axe','Gauntlet','trident']; 
+// var friendName = ['Blake', 'Kayla', 'Cole', 'Mary', 'Josh'];
+// var locationName = ['Hoover','Bessemer','Homewood', 'Mountion Brook','Pelham', 'McCalla','Vestavia Hills','Alabaster','Hueytown','Trussville'];
+// var weaponName = ['Sword','Spear','Hammer','Mace','Fist','Bow','dagger','Sheild','Rod','Staff','Cannons','Whip','Bomb','Axe','Gauntlet','trident','knife']; 
 
 $(document).ready(function() {
-    for(i=1; i <= 100; i++) {
-        $('body').append('<h3 class=h3>Assucation '+ i + ':</h3>');
-        $('.h3').click(getAlert(i));
+
+    // var friendName = ['Blake', 'Kayla', 'Cole', 'Mary', 'Josh'];
+    // var locationName = ['Hoover','Bessemer','Homewood', 'Mountion Brook','Pelham', 'McCalla','Vestavia Hills','Alabaster','Hueytown','Trussville'];
+    // var weaponName = ['Sword','Spear','Hammer','Mace','Fist','Bow','dagger','Sheild','Rod','Staff','Cannons','Whip','Bomb','Axe','Gauntlet','trident','knife']; 
+    
+    //making the 100 accusation 
+    for(i = 1; i <= 100; i++) {
+        // $('body').append('<h3>Accusation '+ i + ':</h3>');
+        // $('h3').click(getAlert(i));
+        var h3 = $('<h3>Accusation ' + i + ':</h3>');
+        $('body').append(h3);
+        $(h3).click(getAlert(i));
+
     };
-
+    
     function getAlert(num) {
-        // var friendName = ['Blake', 'Kayla', 'Cole', 'Mary', 'Josh'];
-        // var locationName = ['Hoover','Bessemer','Homewood', 'Mountion Brook','Pelham', 'McCalla','Vestavia Hills','Alabaster','Hueytown','Trussville'];
-        // var weaponName = ['Sword','Spear','Hammer','Mace','Fist','Bow','dagger','Sheild','Rod','Staff','Cannons','Whip','Bomb','Axe','Gauntlet','trident']; 
-
-        // var name = num % friendName.length;
-        // var location = num % locationName.length;
-        // var weapon = num % weaponName.length; 
+        var friendName = ['Blake', 'Kayla', 'Cole', 'Mary', 'Josh'];
+        var locationName = ['Hoover','Bessemer','Homewood', 'Mountion Brook','Pelham', 'McCalla','Vestavia Hills','Alabaster','Hueytown','Trussville'];
+        var weaponName = ['Sword','Spear','Hammer','Mace','Fist','Bow','dagger','Sheild','Rod','Staff','Cannons','Whip','Bomb','Axe','Gauntlet','trident','knife','needles']; 
         return function() {
             var name = num % friendName.length;
+            console.log(name);
             var location = num % locationName.length;
+            console.log(location); 
             var weapon = num % weaponName.length; 
+            console.log(weapon);
             alert("I accuse "+ friendName[name]+" with the "+ weaponName[weapon]+ " in the "+ locationName[location]+ "!");
         };
-        //return alerting; 
-    }; //return alerting;
+    }; 
 });
+
+console.log('end of scripts');
 
 //I accuse FRIEND_NAME, with the WEAPON_NAME in the LOCATION_NAME!
 
